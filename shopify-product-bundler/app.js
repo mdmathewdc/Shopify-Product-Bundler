67190a2bb014462cb09019d03fb16d15b3c5b71a
mdmathewdc <mathewdony007@gmail.com>
2021-09-07 19:44:54 +1000

    Test server working

--- shopify-product-bundler/app.js
+++ shopify-product-bundler/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello Mathewww!' });
+// If you expect webhook data as JSON
+app.use(express.json());
+// If you expect webhooks as plain text
+app.use(express.text());
+// If you expect webhooks as application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/', (req, res) => {
+    res.send({ message: 'Hello Niceyyy!' });
+    console.log(req.body);
 });
 
 app.listen(3000, () => {
