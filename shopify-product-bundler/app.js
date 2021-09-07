3e456acd8902687d84c79432d7146885bf4ed985
mdmathewdc <mathewdony007@gmail.com>
2021-09-07 19:14:56 +1000

    Added ngrok

--- shopify-product-bundler/app.js
+++ shopify-product-bundler/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello Mathew!' });
+    res.send({ message: 'Hello Mathewww!' });
 });
 
 app.listen(3000, () => {
